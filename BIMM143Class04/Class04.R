6f9c43f94e46a705b64389ec4cb92674719b8506
Anthony Truong <actruong@ucsd.edu>
2026-03-13 11:43:08 -0700

    added multiple classes

---
+++ BIMM143Class04/Class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 04 Lab Intro To R"
+#' author: "Anthony Truong"
+#' date: "January 16th, 2026"
+#' ---
+x <- 1:50
+plot(x)
+plot(x, sin(x),typ ="l", col = "blue", lwd = 3, xlab = "Is it lunch time yet?")
+
